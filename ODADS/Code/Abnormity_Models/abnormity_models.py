fe4ddeab7266e1853f36287465c3ceb9ed543b58
SiQi Pan <SiQiPan2008@Outlook.com>
2024-08-17 16:02:07 +0800

    resize TO_Original images to TO

--- ODADS/Code/Abnormity_Models/abnormity_models.py
+++ ODADS/Code/Abnormity_Models/abnormity_models.py
@@ -11,32 +11,36 @@ def set_parameters_do_not_require_grad(model, featureExtract):
 def initialize_abnormity_model(net_name, num_classes, feature_extract, use_pretrained = True):
     model = None
 
-    if net_name == "152":
-        model = models.resnet152(weights = models.ResNet152_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "101":
-        model = models.resnet101(weights = models.ResNet101_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "050":
-        model = models.resnet50(weights = models.ResNet50_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "034":
-        model = models.resnet34(weights = models.ResNet34_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "018":
-        model = models.resnet18(weights = models.ResNet18_Weights.DEFAULT if use_pretrained else None)
-    
-    elif net_name == "019":
-        model = models.vgg19(weights = models.VGG19_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "016":
-        model = models.vgg16(weights = models.VGG16_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "013":
-        model = models.vgg13(weights = models.VGG13_Weights.DEFAULT if use_pretrained else None)
-    elif net_name == "011":
-        model = models.vgg11(weights = models.VGG11_Weights.DEFAULT if use_pretrained else None)
-    
+    if net_name in ["152", "101", "050", "034", "018"]:
+        if net_name == "152":
+            model = models.resnet152(weights = models.ResNet152_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "101":
+            model = models.resnet101(weights = models.ResNet101_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "050":
+            model = models.resnet50(weights = models.ResNet50_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "034":
+            model = models.resnet34(weights = models.ResNet34_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "018":
+            model = models.resnet18(weights = models.ResNet18_Weights.DEFAULT if use_pretrained else None)
+        set_parameters_do_not_require_grad(model, feature_extract if use_pretrained else False)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Sequential(nn.Linear(num_ftrs, num_classes))
+    elif net_name in ["019", "016", "013", "011"]:
+        if net_name == "019":
+            model = models.vgg19(weights = models.VGG19_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "016":
+            model = models.vgg16(weights = models.VGG16_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "013":
+            model = models.vgg13(weights = models.VGG13_Weights.DEFAULT if use_pretrained else None)
+        elif net_name == "011":
+            model = models.vgg11(weights = models.VGG11_Weights.DEFAULT if use_pretrained else None)
+        set_parameters_do_not_require_grad(model, feature_extract if use_pretrained else False)
+        in_features = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(in_features, num_classes)
     else:
         print("Invalid model name.")
         exit()
     
-    set_parameters_do_not_require_grad(model, feature_extract if use_pretrained else False)
-    num_ftrs = model.fc.in_features
-    model.fc = nn.Sequential(nn.Linear(num_ftrs, num_classes))
+    
     
     return model
--- ODADS/Code/Utils/get_TI.py
+++ ODADS/Code/Utils/get_TI.py
@@ -2,25 +2,52 @@ import os
 import random
 import shutil
 from PIL import Image
-import torchvision.transforms as transforms
 
 # randomly select images from each class in a folder and copy to another folder
+# alternatively, resize all images in each class in a folder to 224*224 and save to another folder  
+
+def resize_long_edge(img, long_edge_size = 224):
+    width, height = img.size
+    if width > height:
+        new_size = (long_edge_size, int(height * long_edge_size / width))
+        loc = (0, int((long_edge_size - new_size[1]) / 2))
+    else:
+        new_size = (int(long_edge_size * width / height), long_edge_size)
+        loc = (int((long_edge_size - new_size[0]) / 2), 0)
+    img = img.resize(new_size)
+    black_background = Image.new("RGB", (long_edge_size, long_edge_size), "black")
+    black_background.paste(img, loc)
+    return black_background
 
 def copy_images(image_list, src_folder, dest_folder):
     for img_name in image_list:
         shutil.copy(os.path.join(src_folder, img_name), os.path.join(dest_folder, img_name))
 
-m = 300
-source_folder = "ODADS/Data/Data/Train/Fundus/"
-target_folder = "ODADS/Data/Data/Test/Fundus/"
+def resize_and_save_images(image_list, src_folder, dest_folder):
+    for img_name in image_list:
+        src_path = os.path.join(src_folder, img_name)
+        dest_path = os.path.join(dest_folder, img_name)
+        img = Image.open(src_path)
+        img = resize_long_edge(img)
+        img.save(dest_path)
+
+
+resize_only = True
+choose_and_copy = False
+source_folder = "ODADS/Data/Images/TO_Original/Fundus/"
+target_folder = "ODADS/Data/Images/TO/Fundus/"
 os.makedirs(target_folder, exist_ok=True)
 all_classes = [f.name for f in os.scandir(source_folder) if f.is_dir()]
 for class_name in all_classes:
     class_source_folder = f'{source_folder}{class_name}'
     class_target_folder = f'{target_folder}{class_name}'
     all_images = os.listdir(class_source_folder)
-    random.shuffle(all_images)
-    all_images = all_images[:m]
     os.makedirs(class_target_folder, exist_ok=True)
-    copy_images(all_images, class_source_folder, class_target_folder)
+    if resize_only:
+        resize_and_save_images(all_images, class_source_folder, class_target_folder)
+    elif choose_and_copy:
+        m = 300
+        random.shuffle(all_images)
+        all_images = all_images[:m]
+        copy_images(all_images, class_source_folder, class_target_folder)
     print(class_name)
