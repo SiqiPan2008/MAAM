2b891a7e42dec13b87cd8133cfabe90591e76aa6
SiQi Pan <SiQiPan2008@Outlook.com>
2024-09-01 14:07:16 +0800

    Complete refining acc and loss graph

--- MAAM/Code/Results/abnormity_loss_and_acc.py
+++ MAAM/Code/Results/abnormity_loss_and_acc.py
@@ -31,7 +31,7 @@ def get_loss_and_acc(net_name, type, starting_epoch = 0):
     test_rs_file = os.path.join(A_folder, setting.get_testing_rs_name(name))
     train_rs = read_csv(train_rs_file)
     test_rs, best_epoch, best_acc = read_csv(test_rs_file)
-    best_epoch = int(best_epoch)
+    best_epoch = int(best_epoch) + starting_epoch
     best_acc = float(best_acc)
     
     epoch_train = [int(row[0]) + starting_epoch for row in train_rs]
@@ -133,14 +133,17 @@ def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
     plt.rcParams['axes.labelsize'] = 9
     plt.rcParams['xtick.labelsize'] = 9
     plt.rcParams['ytick.labelsize'] = 9
+    plt.rcParams['legend.fontsize'] = 9
+    plt.rcParams['lines.linewidth'] = 1
     
     for type in ["AF", "AO"]:
         best_net = setting.best_f_net if type == "AF" else setting.best_o_net
         fig_file = "abnormity_Fundus_loss_and_acc.pdf" if type == "AF" else "abnormity_OCT_loss_and_acc.pdf"
         fig_path = os.path.join(fig_folder, fig_file)
         
-        fig = plt.figure(figsize=(8, 6))
+        fig = plt.figure(figsize=(8, 5) if type == "AO" else (8, 5.4))
         grid = gridspec.GridSpec(2, 2, wspace = 0.05, hspace = 0.25)
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9 if type == "AO" else 0.95, bottom=0.1 if type == "AO" else 0.2092592592592593)
         plt.axis(False)
         plt.title("Fundus" if type == "AF" else "OCT")
         
@@ -153,6 +156,7 @@ def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
             rst = get_loss_and_acc(net_name, type) # ReSults from Transferred learning
             rsf = get_loss_and_acc(net_name, type, starting_epoch = rst["best epoch"]) # ReSults from Finetuning
             continuation_epoch = rst["best epoch"]
+            best_epoch = rst["best epoch"] if rst["best acc"] > rsf["best acc"] else rsf["best epoch"]
             rsf["epoch train"].insert(0, continuation_epoch)
             rsf["train acc"].insert(0, rst["train acc"][continuation_epoch - 1])
             rsf["train losses"].insert(0, rst["train losses"][continuation_epoch - 1])
@@ -162,21 +166,25 @@ def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
             rsf["test acc"].insert(0, rst["test acc"][rst["epoch test"].index(continuation_epoch)])
             rsf["combined acc"].insert(0, rst["combined acc"][rst["epoch test"].index(continuation_epoch)])
             
+            c = ['#0000DD', '#9900EE', '#DD0000', '#DDAA00', '#00DD00', '#00DDEE']
+            
             acc_ax = fig.add_subplot(sub_grid[0, 0])
-            acc_ax.plot(rst["epoch train"], rst["train acc"], color='blue')
-            acc_ax.plot(rsf["epoch train"], rsf["train acc"], color='blue')
-            acc_ax.plot(rst["epoch train"], rst["valid acc"], color='red')
-            acc_ax.plot(rsf["epoch train"], rsf["valid acc"], color='red')
-            #acc_ax.plot(rst["epoch test"], rst["test acc"], color='green')
-            #acc_ax.plot(rsf["epoch test"], rsf["test acc"], color='green')
-            acc_ax.plot(rst["epoch test"], rst["combined acc"], color='green')
-            acc_ax.plot(rsf["epoch test"], rsf["combined acc"], color='green')
+            acc_ax.plot(rst["epoch train"], rst["train acc"], color=c[0])
+            acc_ax.plot(rsf["epoch train"], rsf["train acc"], color=c[1])
+            acc_ax.plot(rst["epoch train"], rst["valid acc"], color=c[2])
+            acc_ax.plot(rsf["epoch train"], rsf["valid acc"], color=c[3])
+            acc_ax.plot(rst["epoch test"], rst["combined acc"], color=c[4])
+            acc_ax.plot(rsf["epoch test"], rsf["combined acc"], color=c[5])
+            acc_ax.axvline(x=continuation_epoch, linewidth = 0.7, color='gray', linestyle='--')
+            acc_ax.axvline(x=best_epoch, linewidth = 0.5, color='gray', linestyle='-')
             
             loss_ax = fig.add_subplot(sub_grid[1, 0])
-            loss_ax.plot(rst["epoch train"], rst["train losses"], color='blue')
-            loss_ax.plot(rsf["epoch train"], rsf["train losses"], color='blue')
-            loss_ax.plot(rst["epoch train"], rst["valid losses"], color='red')
-            loss_ax.plot(rsf["epoch train"], rsf["valid losses"], color='red')
+            loss_ax.plot(rst["epoch train"], rst["train losses"], color=c[0])
+            loss_ax.plot(rsf["epoch train"], rsf["train losses"], color=c[1])
+            loss_ax.plot(rst["epoch train"], rst["valid losses"], color=c[2])
+            loss_ax.plot(rsf["epoch train"], rsf["valid losses"], color=c[3])
+            loss_ax.axvline(x=continuation_epoch, linewidth = 0.7, color='gray', linestyle='--')
+            loss_ax.axvline(x=best_epoch, linewidth = 0.5, color='gray', linestyle='-')
 
             acc_ax.xaxis.set_ticks([])
             if pos[1] == 1:
@@ -186,13 +194,19 @@ def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
                 loss_ax.yaxis.set_label_position('right')
         
         if type == "AF" and pos == (1, 1):
+            
             legend_lines = [
-                Line2D([0], [0], color='blue', lw=2, linestyle='-', label='Training accuracy / loss'),
-                Line2D([0], [0], color='red', lw=2, linestyle='-', label='Validation accuracy / loss'),
-                Line2D([0], [0], color='green', lw=2, linestyle='-', label='Overall accuracy')
+                Line2D([0], [0], color=c[0], linestyle='-', label='Transfer learning training accuracy / loss'),
+                Line2D([0], [0], color=c[2], linestyle='-', label='Transfer learning validation accuracy / loss'),
+                Line2D([0], [0], color=c[4], linestyle='-', label='Transfer learning overall accuracy'),
+                Line2D([0], [0], color="gray", linestyle='--', linewidth = 0.7, label='Finetuning startpoint'),
+                Line2D([0], [0], color=c[1], linestyle='-', label='Finetuning training accuracy / loss'),
+                Line2D([0], [0], color=c[3], linestyle='-', label='Finetuning validation accuracy / loss'),
+                Line2D([0], [0], color=c[5], linestyle='-', label='Finetuning overall accuracy'),
+                Line2D([0], [0], color="gray", linestyle='-', linewidth = 0.5, label='Best model')
             ]
 
-            fig.legend(handles=legend_lines, ncols = 3, bbox_to_anchor = (0.95,0.05))
-            plt.show()
-        
+            fig.legend(handles=legend_lines, ncols = 2, loc = "upper center", bbox_to_anchor = (0.5,0.16))
+            
         plt.savefig(fig_path)
+        #plt.show()
