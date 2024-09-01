3ad6bb62e4dae7d08efd665c5187417abdf3d101
SiQi Pan <SiQiPan2008@Outlook.com>
2024-09-01 18:17:02 +0800

    Refine abnormitise tSNE

--- MAAM/Code/Results/abnormity_tSNE.py
+++ MAAM/Code/Results/abnormity_tSNE.py
@@ -5,9 +5,9 @@ from Utils import utils
 import random
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
-def plot_single_tsne(ax, mr, abnormities, tSNE_point_num):
+def plot_single_tsne(ax, mr, abnormities, tSNE_point_num, add_legends, title):
     shape = mr.shape
     features = []
     labels = []
@@ -27,12 +27,15 @@ def plot_single_tsne(ax, mr, abnormities, tSNE_point_num):
     
     s=2
     scatter = ax.scatter(features_tsne[:, 0], features_tsne[:, 1], c=labels, cmap='jet', marker='o', s=s)
-    for i, disease in enumerate(diseases):
-        handle = Line2D([], [], color=plt.get_cmap('jet')(i / len(diseases)), marker='o', linestyle='None', markersize=s, label=disease)
-        handles.append(handle)
-    plt.legend(handles=handles, bbox_to_anchor=(1, 0.5), loc='center left')
+    if add_legends:
+        handles = []
+        for i, disease in enumerate(abnormities):
+            handle = Line2D([], [], color=plt.get_cmap('jet')(i / len(abnormities)), marker='o', linestyle='None', markersize=s, label=disease)
+            handles.append(handle)
+        ax.legend(handles=handles, bbox_to_anchor=(1.025, 0.5), loc='center left')
     ax.set_xticks([])
     ax.set_yticks([])
+    ax.set_title(title, pad = 10)
 
 def plot_tsne():
     setting = utils.get_setting()
@@ -49,29 +52,33 @@ def plot_tsne():
     f_test_mr_name = setting.get_f_tomr_name(setting.best_f_net)
     o_train_mr_name = setting.get_o_trmr_name(setting.best_o_net)
     f_train_mr_name = setting.get_f_trmr_name(setting.best_f_net)
-    o_abnormities = setting.get_abnormities("OCT Abnormities")
-    f_abnormities = setting.get_abnormities("Fundus Abnormities")
+    o_abnormities = setting.get_abbr_abnormities("OCT Abnormities")
+    o_abnormities = [abnormity[1] for abnormity in o_abnormities]
+    f_abnormities = setting.get_abbr_abnormities("Fundus Abnormities")
+    f_abnormities = [abnormity[1] for abnormity in f_abnormities]
     o_abnormity_num = setting.get_abnormity_num("OCT Abnormities")
     f_abnormity_num = setting.get_abnormity_num("Fundus Abnormities")
     
-    fig = plt.figure(figsize=(9, 8))
+    fig = plt.figure(figsize=(8, 7.5))
     axs = fig.subplots(2, 2)
+    fig.subplots_adjust(wspace = 0.2, hspace = 0.7)
     
     o_train_mr_path = os.path.join(abnormity_folder_path, o_train_mr_name + ".bin")
     o_train_mr = np.fromfile(o_train_mr_path, dtype = np.float64).reshape((o_abnormity_num, o_train_class_size, o_abnormity_num))
-    plot_single_tsne(axs[0, 0], o_train_mr, o_abnormities, tSNE_point_num)
+    plot_single_tsne(axs[0, 0], o_train_mr, o_abnormities, tSNE_point_num, False, "OCT Model Training")
     
     o_test_mr_path = os.path.join(abnormity_folder_path, o_test_mr_name + ".bin")
     o_test_mr = np.fromfile(o_test_mr_path, dtype = np.float64).reshape((o_abnormity_num, o_test_class_size, o_abnormity_num))
-    plot_single_tsne(axs[0, 1], o_test_mr, o_abnormities, tSNE_point_num)
+    plot_single_tsne(axs[0, 1], o_test_mr, o_abnormities, tSNE_point_num, True, "OCT Model Testing")
     
     f_train_mr_path = os.path.join(abnormity_folder_path, f_train_mr_name + ".bin")
     f_train_mr = np.fromfile(f_train_mr_path, dtype = np.float64).reshape((f_abnormity_num, f_train_class_size, f_abnormity_num))
-    plot_single_tsne(axs[1, 0], f_train_mr, f_abnormities, tSNE_point_num)
+    plot_single_tsne(axs[1, 0], f_train_mr, f_abnormities, tSNE_point_num, False, "Fundus Model Training")
     
     f_test_mr_path = os.path.join(abnormity_folder_path, f_test_mr_name + ".bin")
     f_test_mr = np.fromfile(f_test_mr_path, dtype = np.float64).reshape((f_abnormity_num, f_test_class_size, f_abnormity_num))
-    plot_single_tsne(axs[1, 1], f_test_mr, f_abnormities, tSNE_point_num)
+    plot_single_tsne(axs[1, 1], f_test_mr, f_abnormities, tSNE_point_num, True, "Fundus Model Testing")
     
+    plt.tight_layout()
     plt.savefig(fig_path)
     plt.show()
