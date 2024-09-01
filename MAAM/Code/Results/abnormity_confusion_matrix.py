d3f045de295146a2e29c191c69e75426cfdc8fb8
SiQi Pan <SiQiPan2008@Outlook.com>
2024-09-01 17:10:04 +0800

    Refine Abnormities confusion matrices

--- MAAM/Code/Results/abnormity_confusion_matrix.py
+++ MAAM/Code/Results/abnormity_confusion_matrix.py
@@ -25,16 +25,18 @@ def get_complete_conf_mat(mr, abnormity_num):
             conf_mat[i][np.argmax(probs)] += 1
     return conf_mat
 
-def plot_complete_conf_mat(ax, complete_conf_mat, abnormities):
+def plot_complete_conf_mat(ax, complete_conf_mat, abnormities, fontsize, title):
     mat = ax.matshow(complete_conf_mat, cmap='Blues')
     max = np.max(complete_conf_mat)
     for i in range(complete_conf_mat.shape[0]):
         for j in range(complete_conf_mat.shape[1]):
-            ax.text(j, i, complete_conf_mat[i, j], ha='center', va='center', color='black' if complete_conf_mat[i, j] < max / 2 else 'white')
+            ax.text(j, i, complete_conf_mat[i, j], ha='center', va='center', color='black' if complete_conf_mat[i, j] < max / 2 else 'white', fontsize = fontsize)
     ax.set_xticks(np.arange(len(abnormities)))
     ax.set_yticks(np.arange(len(abnormities)))
-    ax.set_xticklabels(abnormities)
+    ax.set_xticklabels(abnormities, rotation=-90)
     ax.set_yticklabels(abnormities)
+    ax.tick_params(axis='x', which='both', bottom=False)
+    ax.set_title(title, fontsize = 12)
 
 def calc_and_save_numerical_results(csv_filename, type, source, conf_mat):
     setting = utils.get_setting()
@@ -93,32 +95,37 @@ def plot_all_conf_mat():
     o_abnormity_num = setting.get_abnormity_num("OCT Abnormities")
     f_abnormity_num = setting.get_abnormity_num("Fundus Abnormities")
     
-    fig = plt.figure(figsize=(9, 8))
+    plt.rcParams['font.size'] = 9
+    plt.rcParams['xtick.labelsize'] = 9
+    plt.rcParams['ytick.labelsize'] = 9
+    
+    fig = plt.figure(figsize=(8, 8))
     axs = fig.subplots(2, 2)
+    fig.subplots_adjust(hspace = 0.35, wspace = 0.2)
     
     o_train_mr_path = os.path.join(abnormity_folder_path, o_train_mr_name + ".bin")
     o_train_mr = np.fromfile(o_train_mr_path, dtype = np.float64).reshape((o_abnormity_num, o_train_class_size, o_abnormity_num))
     o_train_conf_mat = get_complete_conf_mat(o_train_mr, o_abnormity_num)
-    plot_complete_conf_mat(axs[0, 0], o_train_conf_mat, o_abbr_abnormities)
+    plot_complete_conf_mat(axs[0, 0], o_train_conf_mat, o_abbr_abnormities, 6, "OCT Model Training")
     calc_and_save_numerical_results("abnormity_o_train", "OCT", "TR", o_train_conf_mat)
     
     o_test_mr_path = os.path.join(abnormity_folder_path, o_test_mr_name + ".bin")
     o_test_mr = np.fromfile(o_test_mr_path, dtype = np.float64).reshape((o_abnormity_num, o_test_class_size, o_abnormity_num))
     o_test_conf_mat = get_complete_conf_mat(o_test_mr, o_abnormity_num)
-    plot_complete_conf_mat(axs[0, 1], o_test_conf_mat, o_abbr_abnormities)
+    plot_complete_conf_mat(axs[0, 1], o_test_conf_mat, o_abbr_abnormities, 7, "OCT Model Testing")
     calc_and_save_numerical_results("abnormity_o_test", "OCT", "TO", o_test_conf_mat)
     
     f_train_mr_path = os.path.join(abnormity_folder_path, f_train_mr_name + ".bin")
     f_train_mr = np.fromfile(f_train_mr_path, dtype = np.float64).reshape((f_abnormity_num, f_train_class_size, f_abnormity_num))
     f_train_conf_mat = get_complete_conf_mat(f_train_mr, f_abnormity_num)
-    plot_complete_conf_mat(axs[1, 0], f_train_conf_mat, f_abbr_abnormities)
+    plot_complete_conf_mat(axs[1, 0], f_train_conf_mat, f_abbr_abnormities, 8, "Fundus Model Training")
     calc_and_save_numerical_results("abnormity_f_train", "Fundus", "TR", f_train_conf_mat)
     
     f_test_mr_path = os.path.join(abnormity_folder_path, f_test_mr_name + ".bin")
     f_test_mr = np.fromfile(f_test_mr_path, dtype = np.float64).reshape((f_abnormity_num, f_test_class_size, f_abnormity_num))
     f_test_conf_mat = get_complete_conf_mat(f_test_mr, f_abnormity_num)
-    plot_complete_conf_mat(axs[1, 1], f_test_conf_mat, f_abbr_abnormities)
+    plot_complete_conf_mat(axs[1, 1], f_test_conf_mat, f_abbr_abnormities, 9, "Fundus Model Testing")
     calc_and_save_numerical_results("abnormity_f_test", "Fundus", "TO", f_test_conf_mat)
     
-    plt.savefig(fig_path)
+    plt.savefig(fig_path, bbox_inches = "tight")
     plt.show()
