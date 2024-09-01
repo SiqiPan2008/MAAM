1700e4b52eb783d360b60f308a164da77aaa1e05
SiQi Pan <SiQiPan2008@Outlook.com>
2024-09-01 14:13:00 +0800

    Rename acc plot titles

--- MAAM/Code/Results/abnormity_loss_and_acc.py
+++ MAAM/Code/Results/abnormity_loss_and_acc.py
@@ -56,80 +56,17 @@ def get_loss_and_acc(net_name, type, starting_epoch = 0):
         "combined acc": combined_acc
     }
 
-def plot_loss_and_acc_alt(): # (2x1) x (2x2) x (2x1)
-    setting = utils.get_setting()
-    fig_folder = setting.fig_folder
-    fig_file = "abnormity_loss_and_acc.pdf"
-    fig_path = os.path.join(fig_folder, fig_file)
-    
-    fig = plt.figure(figsize=(8, 12))
-    grid = gridspec.GridSpec(2, 1, wspace = 0, hspace = 0.3)
-    plt.axis(False)
-
-    model_list = [
-        ("AF", 0, "152"), 
-        ("AO", 1, "050")
-    ]
-
-    net_list = [
-        ("152", 0, 0), 
-        ("050", 0, 1),
-        ("018", 1, 0),
-        ("016", 1, 1)
-    ]
-    
-    for type, pos, best_net in model_list:
-        sub_ax = fig.add_subplot(grid[pos, 0])
-        sub_ax.set_title("Fundus" if type == "AF" else "OCT")
-        sub_ax.set_axis_off()
-        sub_grid = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec = grid[pos, 0], wspace = 0.3, hspace = 0.3)
-        
-        for net_name, pos_x, pos_y in net_list:
-            subsub_ax = fig.add_subplot(sub_grid[pos_x, pos_y])
-            subsub_ax.set_title(net_name)
-            subsub_ax.set_axis_off()
-            subsub_grid = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec = sub_grid[pos_x, pos_y], wspace = 0, hspace = 0.1)
-            
-            rst = get_loss_and_acc(net_name, type) # ReSults from Transferred learning
-            rsf = get_loss_and_acc(net_name, type, starting_epoch = rst["best epoch"]) # ReSults from Finetuning
-            continuation_epoch = rst["best epoch"]
-            rsf["epoch train"].insert(0, continuation_epoch)
-            rsf["train acc"].insert(0, rst["train acc"][continuation_epoch - 1])
-            rsf["train losses"].insert(0, rst["train losses"][continuation_epoch - 1])
-            rsf["valid acc"].insert(0, rst["valid acc"][continuation_epoch - 1])
-            rsf["valid losses"].insert(0, rst["valid losses"][continuation_epoch - 1])
-            rsf["epoch test"].insert(0, continuation_epoch)
-            rsf["test acc"].insert(0, rst["test acc"][rst["epoch test"].index(continuation_epoch)])
-            
-            acc_ax = fig.add_subplot(subsub_grid[0, 0])
-            acc_ax.plot(rst["epoch train"], rst["train acc"], color='blue')
-            acc_ax.plot(rsf["epoch train"], rsf["train acc"], color='blue')
-            acc_ax.plot(rst["epoch train"], rst["valid acc"], color='red')
-            acc_ax.plot(rsf["epoch train"], rsf["valid acc"], color='red')
-            acc_ax.plot(rst["epoch test"], rst["test acc"], color='green')
-            acc_ax.plot(rsf["epoch test"], rsf["test acc"], color='green')
-            
-            loss_ax = fig.add_subplot(subsub_grid[1, 0])
-            loss_ax.plot(rst["epoch train"], rst["train losses"], color='blue')
-            loss_ax.plot(rsf["epoch train"], rsf["train losses"], color='blue')
-            loss_ax.plot(rst["epoch train"], rst["valid losses"], color='red')
-            loss_ax.plot(rsf["epoch train"], rsf["valid losses"], color='red')
-    
-    plt.show()
-    plt.savefig(fig_path)
-
-
-def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
+def plot_loss_and_acc():
     setting = utils.get_setting()
     fig_folder = setting.fig_folder
     net_list = [
-        ("152", (0, 0)), 
-        ("050", (0, 1)),
-        ("018", (1, 0)),
-        ("016", (1, 1))
+        ("152", "ResNet152", (0, 0)), 
+        ("050", "ResNet50", (0, 1)),
+        ("018", "ResNet18", (1, 0)),
+        ("016", "VGG16", (1, 1))
     ]
     
-    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['axes.titlesize'] = 9
     plt.rcParams['axes.labelsize'] = 9
     plt.rcParams['xtick.labelsize'] = 9
     plt.rcParams['ytick.labelsize'] = 9
@@ -145,11 +82,11 @@ def plot_loss_and_acc(): # 2 x (4x1) x (2x2)
         grid = gridspec.GridSpec(2, 2, wspace = 0.05, hspace = 0.25)
         plt.subplots_adjust(left=0.1, right=0.9, top=0.9 if type == "AO" else 0.95, bottom=0.1 if type == "AO" else 0.2092592592592593)
         plt.axis(False)
-        plt.title("Fundus" if type == "AF" else "OCT")
+        plt.title("Fundus" if type == "AF" else "OCT", fontsize = 12)
         
-        for net_name, pos in net_list:
+        for net_name, net_name_title, pos in net_list:
             sub_ax = fig.add_subplot(grid[pos])
-            sub_ax.set_title(net_name)
+            sub_ax.set_title(net_name_title)
             sub_ax.set_axis_off()
             sub_grid = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec = grid[pos], wspace = 0, hspace = 0.025)
             
